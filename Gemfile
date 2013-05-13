3dd079e32902bf011f88ca5d4dce74da3be51989
Steven Zeiler <zeiler.steven@gmail.com>
2013-05-13 13:40:42 -0500

    add twitter.

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails_admin'
 gem 'omniauth'
 gem 'omniauth-github'
 gem 'omniauth-facebook'
+gem 'omniauth-twitter'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, 'c2d9c8935a890820cb89', 'a6fdeda138f81ed40f9f6b241cf34ea6cfb88651'
   provider :facebook, '407904195945851', '4f333ac04ee12b6d8af459d3fd88faee'
+  provider :twitter, 'M59GguOyWnPviOc0umfALw', '0LBTgZ1S2a6BDz6TJPjYvtWJaySG3zzIFufY5KrNA8'
 end
