39d1d26923ebd77d978c4843509794737c65a4b2
Steven Zeiler <zeiler.steven@gmail.com>
2013-06-01 21:35:37 -0700

    install graphite and add metric

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   def index 
   	@youtube_videos = YoutubeVideo.find([7,1,2,11])
+    Graphite.send_stat('timings.login', 0.5)    
   end 
 
   def contact
---
+++ lib/graphite.rb
@@ -0,0 +1,16 @@
+require 'socket'
+
+class Graphite 
+  class << self 
+    def send_stat(name, value)
+      sock = UDPSocket.new
+      sock.send "#{@api_key}.#{name} #{value}\n", 0, "carbon.hostedgraphite.com", 2003
+    end 
+  end 
+end 
+
+=begin
+  example: 
+
+  Graphite.send_stat('timings.login', 0.5)
+=end 
